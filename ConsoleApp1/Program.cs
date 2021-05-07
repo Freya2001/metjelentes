07c972280dc0465492cab832befe75a1acb2421e
Freay2001 <ehreth.lili@gmail.com>
2021-05-07 15:08:25 +0200

    4es feladat, strukturak

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,16 +6,29 @@ namespace metjelentes
 {
     class Program
     {
+        struct adat
+        {
+            public adat(string ido_, string szel_, int hom_)
+            {
+                ido = ido_;
+                szel = szel_;
+                hom = hom_;
+            }
+            public string ido;
+            public string szel;
+            public int hom;
+        }
         static void Main(string[] args)
         {
             #region 1.feladat
             StreamReader file = new StreamReader("tavirathu13.txt");
             bool readlinesuccesful = true;
             List<string> varos = new List<string>();
-            List<string> ido = new List<string>();
-            List<string> szel = new List<string>();
-            List<int> hom = new List<int>();
-            
+            //List<string> ido = new List<string>();
+            //List<string> szel = new List<string>();
+            //List<int> hom = new List<int>();
+            List<adat> adatok = new List<adat>();
+            Dictionary<string, adat> valogatott = new Dictionary<string, adat>();
             while (readlinesuccesful)
             {
                 string line = file.ReadLine();
@@ -26,10 +39,10 @@ namespace metjelentes
                 }
                 string[] tordeltsor = line.Split(' ');
                 varos.Add(tordeltsor[0]);
-                ido.Add(tordeltsor[1]);
-                szel.Add(tordeltsor[2]);
-                hom.Add(Int32.Parse(tordeltsor[3]));
-
+                //ido.Add(tordeltsor[1]);
+                //szel.Add(tordeltsor[2]);
+                //hom.Add(Int32.Parse(tordeltsor[3]));
+                adatok.Add(new adat(tordeltsor[1], tordeltsor[2], Int32.Parse(tordeltsor[3])));
             }
 
             #endregion
@@ -39,11 +52,11 @@ namespace metjelentes
             string adotttelep = Console.ReadLine();
             
             string legkesobbimeres = "";
-            for (int i = varos.Count-1; i >= 0; i--)
+            for (int i = adatok.Count-1; i >= 0; i--)
             {
                 if (adotttelep == varos[i])
                 {
-                    legkesobbimeres = ido[i];
+                    legkesobbimeres = adatok[i].ido;
                     break;
                 }
             }
@@ -54,31 +67,49 @@ namespace metjelentes
             Console.WriteLine("3.feladat:");
             int maxhomindex = 0;
             int minhomindex = 0;
-            for (int i = 0; i < hom.Count; i++)
+            for (int i = 0; i < varos.Count; i++)
             {
-                if (hom [maxhomindex] < hom[i])
+                if (adatok[maxhomindex].hom < adatok[i].hom)
                 {
                     maxhomindex = i;
                     
                 }
-                if (hom[minhomindex] > hom[i])
+                if (adatok[minhomindex].hom > adatok[i].hom)
                 {
                     minhomindex = i;
                     
                 }
             }
-            Console.WriteLine($"A legalacsonyabb hőmérséklet: {varos[minhomindex]} {ido[minhomindex].Insert(2, ":")} {hom[minhomindex]}  fok.");
-            Console.WriteLine($"A legmagasabb hőmérséklet: {varos[maxhomindex]} {ido[maxhomindex].Insert(2, ":")} {hom[maxhomindex]} fok.");
+            Console.WriteLine($"A legalacsonyabb hőmérséklet: {varos[minhomindex]} {adatok[minhomindex].ido.Insert(2, ":")} {adatok[minhomindex].hom}  fok.");
+            Console.WriteLine($"A legmagasabb hőmérséklet: {varos[maxhomindex]} {adatok[maxhomindex].ido.Insert(2, ":")} {adatok[maxhomindex].hom} fok.");
             Console.Write('\n');
             #endregion
             #region 4.feladat
             Console.WriteLine("4.feladat:");
-
+            bool szelcsend = false;
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                if (adatok[i].szel == "00000")
+                {
+                    Console.WriteLine(varos[i] + " " + adatok[i].ido.Insert(2, ":"));
+                    szelcsend = true;
+                }
+            }
+            if (szelcsend == false)
+            {
+                Console.WriteLine("Nem volt szélcsend a mérések idején.");
+            }
             Console.Write('\n');
             #endregion
             #region 5.feladat
+            Console.WriteLine("5.feladat:");
+
+            Console.Write('\n');
             #endregion
             #region 6.feladat
+            Console.WriteLine("6.feladat:");
+
+            Console.Write('\n');
             #endregion
         }
     }
